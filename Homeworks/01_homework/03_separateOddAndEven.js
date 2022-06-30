269a7445353964f85d3e15e44d9ac8555e1770b8
ath08 <65301780+ath08@users.noreply.github.com>
2022-06-30 18:19:31 +0400

    Update 03_separateOddAndEven.js

--- Homeworks/01_homework/03_separateOddAndEven.js
+++ Homeworks/01_homework/03_separateOddAndEven.js
@@ -1,1 +1,19 @@
+const separateOddAndEven = (arr) => {
+  const even = [];
+  const odd = [];
+  const OddAndEven = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      even.push(arr[i]);
+    }
+    if (arr[i] % 2 === 1) {
+      odd.push(arr[i]);
+    }
+  }
+
+  OddAndEven.push(odd);
+  OddAndEven.push(even);
+  return OddAndEven;
+};
+separateOddAndEven([1, 2, 3, 4, 5, 6, 7, 8]); // => [[1, 3, 5, 7],  [2, 4, 6, 8]]
