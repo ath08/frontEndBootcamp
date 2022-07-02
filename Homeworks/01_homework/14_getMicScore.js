ca5dd709cd28d4badf5e15ef67313164b10cdc88
ath08 <65301780+ath08@users.noreply.github.com>
2022-07-02 10:57:29 +0400

    Create 14_getMicScore.js

---
+++ Homeworks/01_homework/14_getMicScore.js
@@ -0,0 +1,81 @@
+const evaluations = [
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70,
+  },
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60,
+  },
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 60,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 25,
+  },
+];
+
+const getMicScore = (evaluations) => {
+  let minScore = evaluations[0].score;
+  let minScoreStudent;
+
+  for (let i = 0; i < evaluations.length; i++) {
+    if (evaluations[i].score < minScore) {
+      minScore = evaluations[i].score;
+      minScoreStudent = evaluations[i];
+    }
+  }
+  //return minScoreStudent; //return student info who has min scor
+  return minScore; //return only min score value
+};
+
+getMicScore(evaluations); // => 25
