d91fede61d3e6323f3ebf98d370c1ddd004f4726
ath08 <65301780+ath08@users.noreply.github.com>
2022-07-02 23:36:35 +0400

    Create 16_getStudNameMoreComparator.js

---
+++ Homeworks/01_homework/16_getStudNameMoreComparator.js
@@ -0,0 +1,80 @@
+const evaluations = [
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70,
+  },
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60,
+  },
+  {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 60,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50,
+  },
+  {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35,
+  },
+  {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 25,
+  },
+];
+
+const getStudNameMoreComparator = (evaluations, comparator) => {
+  const evaluationsList = [];
+  let studentNameList = [];
+
+  for (let i = 0; i < evaluations.length; i++) {
+    if (evaluations[i].score > comparator) {
+      evaluationsList.push(evaluations[i].studentName);
+      studentNameList = [...new Set(evaluationsList)];
+    }
+  }
+  return studentNameList;
+};
+
+getStudNameMoreComparator(evaluations, 59); // => ['John Doe', 'Tom Colins']
